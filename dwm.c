3fca2a1e1e1258cb6df25811ed02bc8ee1fbf431
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-03-02 19:37:19 +0100

    changed for 240 hz

--- dwm.c
+++ dwm.c
@@ -1166,7 +1166,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 240))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1320,7 +1320,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 240))
 				continue;
 			lasttime = ev.xmotion.time;
 
