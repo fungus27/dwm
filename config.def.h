ec737a091525423184f0dd0600b1d798146ac403
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-03-20 13:22:51 +0000

    various changes + transparent border fix

--- config.def.h
+++ config.def.h
@@ -3,25 +3,21 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 8;       /* snap pixel */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 13;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 13;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 13;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 13;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10", "Material Design Icons:size=12" };
-static const char dmenufont[]       = "JetBrains Mono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#063d8b";
+static const char *fonts[]          = { "dylex terminal:size=9", "Material Design Icons:size=11" };
+static const char dmenufont[]       = "dylex terminal:size=9";
+#include "theme.h"
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_normfg, col_normbg, col_normbord },
+	[SchemeSel]  = { col_selfg, col_main,  col_main  },
 };
 
 /* tagging */
@@ -35,7 +31,9 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	//{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	//{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1, -1, -1, -1, -1, -1 },
+	{ NULL, "feh",     NULL,           0,         1,          1,           0,        -1, 1280, 720, -1, -1, -1 },
+	{ NULL, "mpv",     NULL,           0,         1,          1,           0,        -1, 1280, 720, -1, -1, -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -77,8 +75,8 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	//{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
-	//{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_semicolon,ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_apostrophe, ACTION##stack, {.i = PREVSEL } }, \
 	//{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
 	//{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
 	//{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
@@ -88,7 +86,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_normbg, "-nf", col_normfg, "-sb", col_main, "-sf", col_selfg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "chromium", NULL };
 
@@ -104,7 +102,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                    XK_semicolon, zoom,           {0} },
+	//{ MODKEY,                    XK_semicolon, zoom,           {0} },
 	{ MODKEY|Mod1Mask,              XK_i,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrgaps,       {.i = -1 } },
 	//{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
