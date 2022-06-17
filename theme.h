0a71a8ac580a368bd9cb64d57f8e9027f80b7a68
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-06-17 18:44:31 +0000

    added .gitgnore, theme and ui files

---
+++ theme.h
@@ -0,0 +1,6 @@
+static const char col_normbg[]       = "#050505";
+static const char col_normbord[]       = "#050505";
+static const char col_normfg[]       = "#eeeeee";
+static const char col_selfg[]       = "#ff0000";
+static const char col_selbg[]        = "#050505";
+static const char col_selbord[]        = "#353535";
---
+++ ui.h
@@ -0,0 +1,4 @@
+static const char *fonts[] = {"terminal:size=11" };
+static const char dmenufont[] = "terminal:size=11";
+
+static const unsigned int borderpx = 1;
